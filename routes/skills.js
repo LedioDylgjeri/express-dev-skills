9834d835aa0d77281ae8a292d250afe73cc98040
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2022-09-22 15:38:33 -0400

    create submit form for new skills

--- routes/skills.js
+++ routes/skills.js
@@ -3,9 +3,9 @@ import * as skillsCtrl from '../controllers/skills.js'
 
 const router = Router()
 
-
-/* GET skills listing. */
 router.get('/', skillsCtrl.index)
+
+router.get('/new', skillsCtrl.new)
 
 export {
   router
