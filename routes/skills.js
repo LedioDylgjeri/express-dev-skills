8f803bf5d80a78761c1c455c944572550de8c824
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2022-09-21 21:19:46 -0400

    create controller

--- routes/skills.js
+++ routes/skills.js
@@ -1,12 +1,11 @@
 import { Router } from 'express'
+import { skills } from '../data/skill-data.js'
 
 const router = Router()
 
 
 /* GET skills listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource')
-})
+router.get('/', )
 
 export {
   router
