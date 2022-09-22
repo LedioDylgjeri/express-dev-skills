30dc2252a3ba240252014ee9d8b0c669dc37ee3e
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2022-09-21 23:17:18 -0400

    create model

--- data/skill-data.js
+++ data/skill-data.js
@@ -1,6 +1,6 @@
 const skills = [
-  {text: 'Learn JS', done: true, _id: 123543},
-  {text: 'Learn MongoDB', done: true, _id: 128553},
+  {text: 'Learned JS', done: true, _id: 123543},
+  {text: 'Learned MongoDB', done: true, _id: 128553},
   {text: 'Learned Python', done: false, _id: 133443}
 ]
 
