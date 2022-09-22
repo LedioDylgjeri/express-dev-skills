13497139db11207c0bfd2168f0e5413d3918727f
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2022-09-22 17:41:27 -0400

    implement show functionality

--- routes/skills.js
+++ routes/skills.js
@@ -3,10 +3,14 @@ import * as skillsCtrl from '../controllers/skills.js'
 
 const router = Router()
 
+// GET 
 router.get('/', skillsCtrl.index)
 
 router.get('/new', skillsCtrl.new)
 
+router.get('/:id', skillsCtrl.show)
+
+//POST
 router.post('/', skillsCtrl.create)
 
 export {
